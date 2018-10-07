50029dcb185332e14a111eb618dc55cd66397f80
Jarvis Lam <jarvislam1999@cs.uchicago.edu>
2018-10-07 13:04:19 -0500

    Finished with lab2

--- lab2/integration_lab.py
+++ lab2/integration_lab.py
@@ -6,13 +6,19 @@ def f(x):
     return x*x
 
 
-def integrate():
+def integrate(N):
     ''' Integrate the function f using the rectangle method '''
 
     # Your code here
     # decide on the number of rectangles
+    # N = 1000
     # compute the width of the rectangles
+    dx = 1/N
     # use a loop to compute the total area
+    totalArea = 0
+    for i in range(0,N):
+    	totalArea= totalArea + dx*f(i*dx)
     # return the value of totalArea
+    return totalArea
     # remove the next line
-    pass
+    # pass
--- lab2/list_examples.py
+++ lab2/list_examples.py
@@ -5,3 +5,38 @@ l1 = [1, "abc", 5.7, [1, 3, 5]]
 l2 = [10, 11, 12, 13, 14, 15, 16]
 l3 = [7, -5, 6, 27, -3, 0, 14]
 l4 = [0, 1, 1, 3, 2, 4, 6, 1, 7, 8]
+
+all_pos = True
+for i in l3:
+	if i <0:
+		all_pos = False
+
+pos_only = []
+for i in l3:
+	if i >0:
+		pos_only.append(i)
+
+is_pos_0 = []
+for i in l3:
+	if i >0:
+		is_pos_0.append(True)
+	else:
+		is_pos_0.append(False)
+
+is_pos_1 = []
+for i in range(0,len(l3)):
+	if l3[i] >0:
+		is_pos_1.append(True)
+	else:
+		is_pos_1.append(False)
+
+maxl4 = max(l4)
+
+counts = []
+for i in l4:
+	icount = 0
+	for j in l4:
+		if j == i:
+			icount = icount+1
+	counts.append(icount)
+
