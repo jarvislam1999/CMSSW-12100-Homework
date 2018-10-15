a33d154f8b8c8ee6bbc173a7b77ea41f8249da6b
Jarvis Lam <jarvislam1999@cs.uchicago.edu>
2018-10-15 16:31:40 -0500

    Lab 3 shshould have been released before PA#2 lol

--- lab3/plot_lab.py
+++ lab3/plot_lab.py
@@ -20,6 +20,9 @@ def sinc(x):
         # sin(0) / 0 is defined as the limiting value 1
         return 1.0
 
+def square(x):
+	return x * x
+
 
 def plot_sinc(left_boundary, right_boundary, dx):
     '''
@@ -41,4 +44,22 @@ def plot_sinc(left_boundary, right_boundary, dx):
     pylab.ylabel("sinc(x)")
     pylab.show()
 
+def plot_square(left_boundary, right_boundary, dx):
+    '''
+    Plot the sinc function from left_boundary...right_boundary with
+    increments of size dx.
+    '''
+    xs = numpy.arange(left_boundary, right_boundary, dx) 
 
+    # apply the sinc function onto the xs list
+    ys = []
+    for x in xs:
+        ys.append(square(x))
+                
+    # plot the figure
+    pylab.figure()
+    pylab.plot(xs,ys)
+    pylab.title("Square function")
+    pylab.xlabel("X values")
+    pylab.ylabel("x ** 2")
+    pylab.show()
